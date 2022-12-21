ef575758fa2809d262942ab194846f0061ee0e88
Archer <120321858+LuisRamosOfficial@users.noreply.github.com>
2022-12-21 19:13:29 +0000

    Exchange Calculation Complete

--- exchange_rate_js/pages/index.tsx
+++ exchange_rate_js/pages/index.tsx
@@ -1,15 +1,25 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/index.module.css';
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 import exchangePic from '../public/exchange.jpg';
+import RefreshPic from '../public/refresh-icon.webp';
 import Aos from 'aos';
 import { GetServerSideProps } from 'next';
 import 'aos/dist/aos.css';
-import CachedIcon from '@mui/icons-material/Cached';
-import absoluteUrl from 'next-absolute-url';
+import React from 'react';
 
-export default function Home({ data }: any) {
+interface Props {
+	children?: React.ReactNode;
+	data: Object;
+	names: Array<string>;
+	acro: Array<string>;
+}
+interface Children {
+	children: React.ReactNode;
+}
+
+const Home: FC<Props> = ({ data, names, acro }) => {
 	useEffect(() => {
 		Aos.init({ duration: 2000 });
 	}, []);
@@ -42,50 +52,117 @@ export default function Home({ data }: any) {
 	return (
 		<main className={styles.main}>
 			<Presentation />
-			<ExchangingSection data={data} />
+			<ExchangingSection data={data} names={names} acro={acro} />
 		</main>
 	);
-}
+};
 
-const ExchangingSection = ({ data }: any) => {
+const ExchangingSection: FC<Props> = ({ data, names, acro }: Props) => {
+	const [Value, setValue] = useState<number>(0);
+	const [Response, setResponse] = useState<number>(0);
+	const [Lcurrency, setLcurrency] = useState<number>(0);
+	const [Rcurrency, setRcurrency] = useState<number>(0);
 
-	console.log(data)
+	useEffect(() => {
+		const Lrate = data[acro[Lcurrency ] as keyof typeof data]['rate' as keyof typeof data]
+		const Rrate = data[acro[Rcurrency ] as keyof typeof data]['rate' as keyof typeof data]
+		const calculation = (Value * Number(Rrate)) / Number(Lrate)
+		setResponse(Math.round((calculation + Number.EPSILON) * 10000) / 10000);
+	
+
+	}, [Value, Lcurrency, Rcurrency])
+	
+
+	const SwitchButton: FC = () => {
+
+		const Switch = () => {
+			let R: number = Rcurrency;
+			let L: number = Lcurrency;
+
+			setLcurrency(R)
+			setRcurrency(L)
+		}
+		return (
+			<button onClick={() => Switch()} className={styles.switchButton}>
+				<div className={styles.imagewrapper}>
+					<Image
+						src={RefreshPic}
+						alt="Landscape picture"
+						width={50}
+						height={50}
+					/>
+				</div>
+			</button>
+		);
+	};
+
 	return (
 		<section className={styles.ExchangingSection}>
 			<h1>Exchange Now!</h1>
 			<div className={styles.BoxExchange}>
 				<span>
 					<p>From:</p>
-					<select name={'currencys'}>
-						<option value="volvo">Volvo</option>
-						<option value="saab">Saab</option>
-						<option value="opel">Opel</option>
-						<option value="audi">Audi</option>
+					<select
+						value={Lcurrency}
+						onChange={(e) => {
+							setLcurrency(parseInt(e.target.value));
+						}}
+						name={'currencys'}
+					>
+						{names.map((element, index) => (
+							<option value={index}>{element}</option>
+						))}
 					</select>
 				</span>
-				<CachedIcon />
+				<SwitchButton />
 				<span>
 					<p>To:</p>
-					<select name={'currencys'}>
-						<option value="volvo">Volvo</option>
-						<option value="saab">Saab</option>
-						<option value="opel">Opel</option>
-						<option value="audi">Audi</option>
+					<select
+						value={Rcurrency}
+						onChange={(e) => {
+							setRcurrency(parseInt(e.target.value));
+						}}
+						name={'currencys'}
+					>
+						{names.map((element, index) => (
+							<option value={index}>{element}</option>
+						))}
 					</select>
 				</span>
+				<input
+					type="number"
+					onChange={(e) => {
+						setValue(parseFloat(e.target.value));
+					}}
+				/>
+				<input
+					type="number"
+					value={Response}
+					disabled
+					className={styles.disabled}
+				/>
+				{/* <input /> */}
 			</div>
 		</section>
 	);
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-	const url: any = context.req.client._httpMessage.req.rawHeaders[1];
+	const res = await fetch('http://localhost:3000/api/currency_list');
+	const data = await res.json();
 
-	const res = await fetch('http://' +url + '/api/currency_list')
-	const data = await res.json()
+
+	const res_acro = await fetch('http://localhost:3000/api/currency_list/acronyms');
+	const acro = await res_acro.json();
+	let names = [];
+
+	for (const key in data) {
+		names.push(data[key].name);
+	}
 
 	return {
-		props: {data},
+		props: { data, names, acro },
 	};
 };
 
+export default Home;
