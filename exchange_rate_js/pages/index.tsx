1fef18471dd62858fe4223c8ae207e5409133a78
Archer <120321858+LuisRamosOfficial@users.noreply.github.com>
2022-12-21 00:49:43 +0000

    API Builded

--- exchange_rate_js/pages/index.tsx
+++ exchange_rate_js/pages/index.tsx
@@ -1,23 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/index.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/index.module.css';
 import { FC, useEffect } from 'react';
-import exchangePic from '../public/exchange.jpg'
+import exchangePic from '../public/exchange.jpg';
 import Aos from 'aos';
+import { GetServerSideProps } from 'next';
 import 'aos/dist/aos.css';
+import CachedIcon from '@mui/icons-material/Cached';
+import absoluteUrl from 'next-absolute-url';
 
+export default function Home({ data }: any) {
+	useEffect(() => {
+		Aos.init({ duration: 2000 });
+	}, []);
 
-
-export default function Home() {
-
-
-	 useEffect(() => {
-			Aos.init({ duration: 2000 });
-
-		}, []);
-  
-  const Presentation: FC = () => {
-    return (
+	const Presentation: FC = () => {
+		return (
 			<div className={styles.presentation}>
 				<div className={styles.imagewrapper}>
 					<Image
@@ -39,13 +37,55 @@ export default function Home() {
 				</p>
 			</div>
 		);
-  }
-  
+	};
 
-  return (
-    <main className={styles.main}>
-      <Presentation />
-    </main>
-  )
+	return (
+		<main className={styles.main}>
+			<Presentation />
+			<ExchangingSection data={data} />
+		</main>
+	);
 }
+
+const ExchangingSection = ({ data }: any) => {
+
+	console.log(data)
+	return (
+		<section className={styles.ExchangingSection}>
+			<h1>Exchange Now!</h1>
+			<div className={styles.BoxExchange}>
+				<span>
+					<p>From:</p>
+					<select name={'currencys'}>
+						<option value="volvo">Volvo</option>
+						<option value="saab">Saab</option>
+						<option value="opel">Opel</option>
+						<option value="audi">Audi</option>
+					</select>
+				</span>
+				<CachedIcon />
+				<span>
+					<p>To:</p>
+					<select name={'currencys'}>
+						<option value="volvo">Volvo</option>
+						<option value="saab">Saab</option>
+						<option value="opel">Opel</option>
+						<option value="audi">Audi</option>
+					</select>
+				</span>
+			</div>
+		</section>
+	);
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	const url: any = context.req.client._httpMessage.req.rawHeaders[1];
+
+	const res = await fetch('http://' +url + '/api/currency_list')
+	const data = await res.json()
+
+	return {
+		props: {data},
+	};
+};
 
