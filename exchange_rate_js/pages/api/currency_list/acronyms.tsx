39b35de8fb198e4a4d828d8c92eb97a85b7e3a25
Archer <120321858+LuisRamosOfficial@users.noreply.github.com>
2022-12-22 14:47:19 +0000

    Test

--- exchange_rate_js/pages/api/currency_list/acronyms.tsx
+++ exchange_rate_js/pages/api/currency_list/acronyms.tsx
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+// test
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
 	res.status(200).json(['GBP', 'USD', 'EUR', 'STN', 'BRL']);
 }
